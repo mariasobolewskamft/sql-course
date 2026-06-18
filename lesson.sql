b8022b58b86d7986e5f1bf9324c5a4f731b4dd8e
mariasobolewskamft <maria.sobolewska@mft.nhs.uk>
2026-06-18 11:23:50 +0000

    end of morning

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,27 @@
 /*
-Example lesson stub
+I can put a long bit in 
+here
+lots of notes
 */
-
+--looking at surgical activity
 SELECT
-	*
+	ps.PatientId 
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,DATEADD (WEEK, -2, ps.AdmittedDate) AS ReminderDate
+	
+	,ps.AdmittedDate
+	
+	,ps.dischargedate
+	
+	,ps.tariff
 FROM
-	PatientStay ps ;
+	PatientStay ps
+WHERE ps.Hospital IN ('kingston','pruh')
+	AND ps.Ward LIKE '%surgery'
+	AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-02-29'
+
+
